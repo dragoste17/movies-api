931a7c2b3afce7f09901c92dd561311185581b2c
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-10-18 23:39:29 +0900

    Create Movies Table

---
+++ database/migrations/2020_10_18_141728_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('release_date');
+            $table->string('original_language');
+            $table->integer('runtime');
+            $table->enum('rating', ['G', 'PG', 'PG-13', 'R', 'NC-17']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
