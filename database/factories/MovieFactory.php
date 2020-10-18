f1d26b2f5981112c004994f953bfcd612d330b93
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-10-19 00:13:44 +0900

    Create Movie Factory

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MovieFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Movie::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(rand(1, 4), true),
+            'release_date' => $this->faker->date,
+            'original_language' => $this->faker->languageCode,
+            'runtime' => $this->faker->numberBetween(80, 180),
+            'rating' => $this->faker->randomElement(['G', 'PG', 'PG-13', 'R', 'NC-17']),
+        ];
+    }
+}
