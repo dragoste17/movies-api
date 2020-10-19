57a95133b192afa61912da40620e7cc7b958ef6a
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-10-19 22:04:37 +0900

    Create Basic Movies Controller and Routes

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        return Movie::all();
+    }
+
+    public function show(Movie $movie)
+    {
+        return $movie;
+    }
+}
