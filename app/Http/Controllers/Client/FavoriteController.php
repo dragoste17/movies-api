d9ed42e665f00603df0417bcb7fe3a986a9822c2
Lav Shinde <13927503+dragoste17@users.noreply.github.com>
2020-10-20 12:32:07 +0900

    Update FavoriteController to use Internal APIs

--- app/Http/Controllers/Client/FavoriteController.php
+++ app/Http/Controllers/Client/FavoriteController.php
@@ -3,18 +3,39 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Favorite;
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class FavoriteController extends Controller
 {
     public function index(Request $request)
     {
-        return $request->user()->favorites;
+        $favorites = Favorite::whereUserId($request->user()->id)->get();
+        if ($favorites->isEmpty()) {
+            return response()->json([]);
+        }
+        $favoriteMovieIds = $favorites->pluck('movie_id')->toArray();
+        $favoriteMovies = $this->getMoviesById($favoriteMovieIds);
+        return $favoriteMovies;
     }
 
-    public function store(Request $request, Movie $movie)
+    private function getMoviesById($ids)
     {
-        $request->user()->addToFavorites($movie);
+        $respose = Http::get('http://movies-api-nginx/api/internal/movies', [
+            'apiKey' => config('auth.api_key'),
+            'isFetchById' => 'true',
+            'movieIds' => $ids,
+        ]);
+        return $respose->json();
+    }
+
+    public function store(Request $request, $movieId)
+    {
+        $favorite = new Favorite;
+        $favorite->user_id = $request->user()->id;
+        $favorite->movie_id = $movieId;
+        $favorite->save();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware([VerifyApiKey::class])->prefix('internal/movies')->group(funct
 
 // Route::middleware('auth:api')->group(function () {
     Route::get('/favorites', [FavoriteController::class, 'index']);
-    Route::post('/favorite/{movie}', [FavoriteController::class, 'store']);
+    Route::post('/favorite/{movieId}', [FavoriteController::class, 'store']);
     Route::get('/movies', [ClientMovieController::class, 'index']);
     Route::get('/movie/{movieId}', [ClientMovieController::class, 'show']);
 // });
